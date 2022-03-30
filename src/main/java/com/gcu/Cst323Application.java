1e43889539d278ed9570f1c2c85a2d27b7424d61
Ivan-RC <rivas.ivan468@gmail.com>
2022-03-30 13:03:00 -0700

    removed cst323activity file

--- src/main/java/com/gcu/Cst323Application.java
+++ src/main/java/com/gcu/Cst323Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Cst323ActivityApplication {
+public class Cst323Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Cst323ActivityApplication.class, args);
+		SpringApplication.run(Cst323Application.class, args);
 	}
 
 }
