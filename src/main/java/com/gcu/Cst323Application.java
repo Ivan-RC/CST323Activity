20082270128be962dfda97b2b51c6ee964359d5c
Ivan-RC <46502817+Ivan-RC@users.noreply.github.com>
2022-03-30 12:42:40 -0700

    Update and rename Cst323ActivityApplication.java to Cst323Application.java

--- src/main/java/com/gcu/Cst323Application.java
+++ src/main/java/com/gcu/Cst323Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Cst323ActivityApplication {
+public class Cst323Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cst323ActivityApplication.class, args);
