cd08388df4520d50e6c29e46689a232abb8456c6
Ivan-RC <46502817+Ivan-RC@users.noreply.github.com>
2022-03-30 13:00:34 -0700

    Update Cst323Application.java

--- src/main/java/com/gcu/Cst323Application.java
+++ src/main/java/com/gcu/Cst323Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Cst323Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Cst323ActivityApplication.class, args);
+		SpringApplication.run(Cst323Application.class, args);
 	}
 
 }
